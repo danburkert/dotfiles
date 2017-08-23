e9a5576001bd998e38f96e943df61b1f5db5dbb5
Dan Burkert <dan@danburkert.com>
2017-08-22 17:59:50 -0700

    update init.fish

--- init.fish
+++ init.fish
@@ -1,1 +1,8 @@
+set -xg KUDU_HOME $HOME/src/cpp/kudu
+set -x PATH /usr/local/opt/ccache/libexec $HOME/opt/bin $HOME/.cargo/bin $PATH
+
+alias git=hub
+alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+
 set -g theme_display_user yes
+ulimit -n 2048
