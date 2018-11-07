5228e43c6785f07ea93a78c09611d431b67b34eb
Dan Burkert <dan@danburkert.com>
2018-11-06 23:01:48 -0800

    Add config.fish to install

--- config.fish
+++ config.fish
@@ -1,10 +1,8 @@
-set -xg KUDU_HOME $HOME/src/cpp/kudu
-set -x PATH /usr/local/opt/ccache/libexec $HOME/opt/bin $HOME/.cargo/bin $PATH
+# Add to default $PATH
+# set --universal fish_user_paths $fish_user_paths $HOME/.cargo/bin
+
 set -xg FZF_DEFAULT_COMMAND 'rg --files'
 
 alias git=hub
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 alias ll='exa -lh'
-
-set -g theme_display_user yes
-ulimit -n 2048
