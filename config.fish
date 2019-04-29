d9469ca23252bae8814e7ff0dcab849e29664f5b
Dan Burkert <dan@danburkert.com>
2019-04-29 20:44:59 +0000

    only source rbenv when available

--- config.fish
+++ config.fish
@@ -1,13 +1,12 @@
-# Add to default $PATH
-# set --universal fish_user_paths $fish_user_paths $HOME/.cargo/bin
-
 set -xg FZF_DEFAULT_COMMAND 'rg --files'
 set -xg RIPGREP_CONFIG_PATH $HOME/.ripgreprc
 
 alias git=hub
 alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 alias ll='exa -lh'
-set -g fish_user_paths "/usr/local/sbin" $fish_user_paths
+
+# Add to default $PATH
+set -g fish_user_paths $fish_user_paths /usr/local/sbin $HOME/.cargo/bin $HOME/bin
 
 # Add rbenv to the path.
-status --is-interactive; and source (rbenv init -|psub)
+status --is-interactive; and type -q rbenv; and source (rbenv init -|psub)
