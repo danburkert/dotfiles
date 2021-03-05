0763045bf9314a198a340f0506cd9db149ea3964
Dan Burkert <dan@danburkert.com>
2021-03-04 22:20:59 -0800

    fixups

--- config.fish
+++ config.fish
@@ -1,19 +1,32 @@
 set -xg FZF_DEFAULT_COMMAND 'rg --files'
 set -xg RIPGREP_CONFIG_PATH $HOME/.ripgreprc
-# set -xg JAVA_HOME (/usr/libexec/java_home)
 
-# alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
 alias ll='exa -lh'
 alias cat=bat
 alias vim=nvim
 
-# Add to default $PATH
-set -g fish_user_paths $fish_user_paths /usr/local/sbin $HOME/.cargo/bin $HOME/bin
-
 # Add Helm to the path.
-# set -g fish_user_paths "/usr/local/opt/helm@2/bin" $fish_user_paths
+for bin_dir in /usr/local/opt/helm@2/bin /usr/local/opt/postgresql@10/bin
+  if test -d $bin_dir
+    set -g fish_user_paths $bin_dir $fish_user_paths
+  end
+end
 
 # Add PostgreSQL 10 to the path
-# set -g fish_user_paths "/usr/local/opt/postgresql@10/bin" $fish_user_paths
+set -g fish_user_paths "" $fish_user_paths
 
 starship init fish | source
+
+if test -x /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession
+  alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
+end
+
+if test -x /usr/libexec/java_home
+  set -xg JAVA_HOME (/usr/libexec/java_home)
+end
+
+# pyenv init
+if command -v pyenv 1>/dev/null 2>&1
+  pyenv init - | source
+end
+
