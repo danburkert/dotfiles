db046fe2992d6811cd3e3e11d4d729b231856c8b
Dan Burkert <dan@danburkert.com>
2021-03-06 11:19:35 -0800

    fix fish paths

--- config.fish
+++ config.fish
@@ -5,17 +5,11 @@ alias ll='exa -lh'
 alias cat=bat
 alias vim=nvim
 
-# Add Helm to the path.
-for bin_dir in /usr/local/opt/helm@2/bin /usr/local/opt/postgresql@10/bin
+for bin_dir in /usr/local/opt/helm@2/bin /usr/local/opt/postgresql@10/bin $HOME/.cargo/bin $HOME/bin
   if test -d $bin_dir
     set -g fish_user_paths $bin_dir $fish_user_paths
   end
 end
-
-# Add PostgreSQL 10 to the path
-set -g fish_user_paths "" $fish_user_paths
-
-starship init fish | source
 
 if test -x /System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession
   alias lock="/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend"
@@ -25,8 +19,8 @@ if test -x /usr/libexec/java_home
   set -xg JAVA_HOME (/usr/libexec/java_home)
 end
 
-# pyenv init
 if command -v pyenv 1>/dev/null 2>&1
   pyenv init - | source
 end
 
+starship init fish | source
